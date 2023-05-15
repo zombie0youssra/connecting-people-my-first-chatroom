4c13d5551995519cef9e78f50a2ddd23f92708b3
youssra <112857270+zombie0youssra@users.noreply.github.com>
2023-05-15 12:04:42 +0200

    gestolen van justus

---
+++ public/script.js
@@ -0,0 +1,32 @@
+let socket = io();
+let messages = document.querySelector("section ul");
+let input = document.querySelector("input");
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (input.value) {
+    socket.emit("message", input.value);
+    input.value = "";
+  }
+});
+
+socket.on("message", (message) => {
+  addMessage(message);
+});
+
+socket.on("whatever", (message) => {
+  addMessage(message);
+});
+
+socket.on("history", (history) => {
+  history.forEach((message) => {
+    addMessage(message);
+  });
+});
+
+function addMessage(message) {
+  messages.appendChild(
+    Object.assign(document.createElement("li"), { textContent: message })
+  );
+  messages.scrollTop = messages.scrollHeight;
+}
